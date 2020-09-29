f89de8e467c15623a197f27f36d8a4fe997ff0ec
Bohan Zhang <bohan.zhang.1@slu.edu>
2020-09-29 08:39:42 -0500

    Initial commit

---
+++ Assets/Scripts/BaseHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseHealth : MonoBehaviour
+{
+    public float health = 2;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
